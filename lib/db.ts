07d40c5e1283522f012e447b8ae55366248ef0ee
Temitope8-tech <temitopeomotolani502@gmail.com>
2026-09-11 05:38:12 -0700

    Fix production Temporal runtime

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,4 @@
+import "temporal-polyfill";
 import postgres from "@prisma/orm-postgres/runtime";
 import type { Contract } from "../prisma/contract.d";
 import contractJson from "../prisma/contract.json" with { type: "json" };
