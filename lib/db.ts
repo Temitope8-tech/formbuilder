5917a7531497aa9ed43e0faa7bb235bb1c36fcfa
Temitope8-tech <temitopeomotolani502@gmail.com>
2026-09-11 05:50:45 -0700

    Load Temporal globally

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import "temporal-polyfill";
+import "temporal-polyfill/global";
 import postgres from "@prisma/orm-postgres/runtime";
 import type { Contract } from "../prisma/contract.d";
 import contractJson from "../prisma/contract.json" with { type: "json" };
